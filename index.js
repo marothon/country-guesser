b888c701c990eb73061c6bb6f84d7c2e7d5446ae
boelm <boel_mattsson@hotmail.com>
2024-08-01 12:08:48 +0200

    test version OK. Something to show my team mate :-)

--- index.js
+++ index.js
@@ -35,3 +35,55 @@ async function performSearch() {
         console.error(error);
     }
 }
+
+//array of countries -> local storage?
+//cca2, common, nativeName-array
+
+//function randomize array of countries
+
+console.log("Hej JS-filen!");
+
+const urlAllCountries = "https://restcountries.com/v3.1/all";
+
+
+let countryList = getAllCountries(urlAllCountries);
+
+function toNunzia(){
+    getAllCountries(url);
+}
+
+async function getAllCountries(url) {
+  let countries = await fetchData(url);
+  let countryList = [];
+  //console.log(countries);
+  countries.forEach((country) => {
+    let newCountry = { name: country.name.common, cca2: country.cca2 };
+    countryList.push(newCountry);
+  });
+  localStorage.setItem("countryListOrdered", JSON.stringify(countryList));
+  let randomizedList = shuffleList(countryList);
+  localStorage.setItem(
+    "countryListRandomOrder",
+    JSON.stringify(randomizedList)
+  );
+  return countryList;
+}
+
+async function fetchData(url) {
+  let data = [];
+  try {
+    let response = await fetch(url);
+    let data = await response.json();
+    return data;
+  } catch (error) {}
+}
+
+//Randomizes order of a list.
+//Fisher-Yates Sorting Algorithm (https://freecodecamp.org/news/how-to-shuffle-an-array-of-items-using-javascript-or-typescript/)
+function shuffleList(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
