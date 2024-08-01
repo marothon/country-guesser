cef36d57768daf0ddeabc90cc7ffefeb20824a6f
Sara Vahidinovin <sa.vahidinovin@gmail.com>
2024-08-01 12:56:40 +0200

    Added search input (#13)
    Closes #3.
    
    * Added search input
    * Added performSearch function by enter
    * Added search input style
    * Fetching and filtering names

--- index.js
+++ index.js
@@ -12,3 +12,26 @@ function countryClickListener(event){
     const countryName = event.target.getAttribute('title');
     document.getElementById('guessed-country').innerHTML = `You guessed ${countryName}`;
 }
+
+
+//Search countries
+
+document.getElementById('searchInput').addEventListener('keydown', function(event) {
+    if (event.key === 'Enter') {
+        performSearch();
+    }
+});
+
+async function performSearch() {
+    const searchText = document.getElementById('searchInput').value.toLowerCase();
+    try {
+        const response = await fetch('https://restcountries.com/v3.1/name/' + searchText);
+        const data = await response.json();
+        const filteredCountries = data.filter(country => country.name.common.toLowerCase().startsWith(searchText));
+        if (filteredCountries.length > 0) {
+            console.log(filteredCountries);
+        }
+    } catch (error) {
+        console.error(error);
+    }
+}
