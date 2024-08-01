2774147838dc0dd6a4a44a24d236daea4c62d541
marothon <marothon@gmail.com>
2024-08-01 09:45:31 +0200

    Add click listeners to each country path and a hover effect

--- index.js
+++ index.js
@@ -2,4 +2,13 @@ setupWorldMap();
 
 function setupWorldMap(){
     document.getElementById('world-map-container').innerHTML = worldMap;
+    document.querySelectorAll("#world-map-container > svg > path").forEach(
+        (countryPath) => countryPath.addEventListener('click', countryClickListener)
+    );
+}
+
+function countryClickListener(event){
+    const isoCode2 = event.target.id;
+    const countryName = event.target.getAttribute('title');
+    document.getElementById('guessed-country').innerHTML = `You guessed ${countryName}`;
 }
